a6a0d2f92651e931f16dec51b5389f5c61818d77
wftmate <williamfitchturner@gmail.com>
2019-07-15 13:44:51 -0400

    added new class for keyEventReceiver

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -78,39 +78,39 @@ bool MainWindow::EventFilter(QEvent *event){
 // -- Attempt 1 to catch "Enter"/"Return" key ---
 
 
-// -- Attempt 2 to catch "Enter"/"Return" key --- I think this one is better
+//// -- Attempt 2 to catch "Enter"/"Return" key --- I think this one is better
 
-// Define event filter class - this probably belongs in a different .cpp file
-class keyEnterReceiver : public QObject
-{
-    Q_OBJECT
-protected:
-    bool eventFilter(QObject* obj, QEvent* event);
-};
+//// Define event filter class - this probably belongs in a different .cpp file
+//class keyEnterReceiver : public QObject
+//{
+//    Q_OBJECT
+//protected:
+//    bool eventFilter(QObject* obj, QEvent* event);
+//};
 
-// Define what the eventFilter function beloning to the keyEnterReciever class does
-bool keyEnterReceiver::eventFilter(QObject* obj, QEvent* event)
-{
-    if (event->type()==QEvent::KeyPress) {
-        QKeyEvent* key = static_cast<QKeyEvent*>(event);
-        if ( (key->key()==Qt::Key_Enter) || (key->key()==Qt::Key_Return) ) {
-            //Enter or return was pressed
+//// Define what the eventFilter function beloning to the keyEnterReciever class does
+//bool keyEnterReceiver::eventFilter(QObject* obj, QEvent* event)
+//{
+//    if (event->type()==QEvent::KeyPress) {
+//        QKeyEvent* key = static_cast<QKeyEvent*>(event);
+//        if ( (key->key()==Qt::Key_Enter) || (key->key()==Qt::Key_Return) ) {
+//            //Enter or return was pressed
 
-            // how do I make this class do stuff in another class
+//            // how do I make this class do stuff in another class
 
-            MainWindow::EnterPressed();
-        } else {
-            return QObject::eventFilter(obj, event);
-        }
-        return true;
-    } else {
-        return QObject::eventFilter(obj, event);
-    }
-    return false;
-}
+//            MainWindow::EnterPressed();
+//        } else {
+//            return QObject::eventFilter(obj, event);
+//        }
+//        return true;
+//    } else {
+//        return QObject::eventFilter(obj, event);
+//    }
+//    return false;
+//}
 
-// Install the keyEnterReciever event filter onto the appropriate widget (which one is that?)
-keyEnterReceiver* key = new keyEnterReceiver();
-aWidgetInAnotherClass->installEventFilter(key);
+//// Install the keyEnterReciever event filter onto the appropriate widget (which one is that?)
+//keyEnterReceiver* key = new keyEnterReceiver();
+//aWidgetInAnotherClass->installEventFilter(key);
 
-// -- Attempt 2 to catch "Enter"/"Return" key ---
+//// -- Attempt 2 to catch "Enter"/"Return" key --- I think this one is better
